de7392fb61402c6aed9791f3118d1242f2f614a8
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2021-09-26 23:25:30 -0300

    Test: Card de Relatório com 'valores calculados'

--- src/screens/Principal/index.js
+++ src/screens/Principal/index.js
@@ -1,18 +1,82 @@
 /* eslint-disable prettier/prettier */
-import React, {useState, useEffect, useContext} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Appbar} from 'react-native-paper';
+import React, {useContext} from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {Appbar, Button, Card, Paragraph, Title} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
 import { TesteContext } from '../../providers';
 
 export default function () {
-  const {dados,setDados} = useContext(TesteContext)
+  const { dados, setDados } = useContext(TesteContext);
+
+  const iluminacao = 3225;
+  const parede = 3225;
+  const equipamentos = 3225;
+  const teto = 3225;
+  const pessoas = 3225;
   
   const navegar = useNavigation();
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text></Text>
+    <>
+      <ScrollView>
+        <View>
+
+      <Card style={{marginBottom: 5}}>
+        <Card.Title title="Iluminação" subtitle="Carga resultante da iluminação do ambiente"/>
+        <Card.Content style={{alignItems: 'center'}}>
+          <Title>{ iluminacao }</Title>
+        </Card.Content>
+        <Card.Actions>
+          <Button>Limpar</Button>
+        </Card.Actions>
+      </Card>
+
+      <Card style={{ marginBottom: 5 }}>
+        <Card.Title title="Parede" subtitle="Carga resultante da condução das Paredes" />
+        <Card.Content style={{ alignItems: 'center' }}>
+          <Title>{parede}</Title>
+        </Card.Content>
+        <Card.Actions>
+          <Button>Limpar</Button>
+        </Card.Actions>
+      </Card>
+
+      <Card style={{ marginBottom: 5 }}>
+        <Card.Title title="Teto" subtitle="Carga resultante da condução do Teto" />
+        <Card.Content style={{ alignItems: 'center' }}>
+          <Title>{teto}</Title>
+        </Card.Content>
+        <Card.Actions>
+          <Button>Limpar</Button>
+        </Card.Actions>
+      </Card>
+
+      <Card style={{ marginBottom: 5 }}>
+        <Card.Title title="Equipamentos" subtitle="Carga resultante dos equipamentos" />
+        <Card.Content style={{ alignItems: 'center' }}>
+          <Title>{equipamentos}</Title>
+        </Card.Content>
+        <Card.Actions>
+          <Button>Limpar</Button>
+        </Card.Actions>
+      </Card>
+
+      <Card style={{ marginBottom: 5 }}>
+        <Card.Title title="Pessoas" subtitle="Carga resultante das pessoas" />
+        <Card.Content style={{ alignItems: 'center' }}>
+          <Title>{pessoas}</Title>
+        </Card.Content>
+        <Card.Actions>
+          <Button>Limpar</Button>
+        </Card.Actions>
+      </Card>
+          </View>
+
+      </ScrollView>
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+
+
       <View style={styles.bottom}>
         <Appbar
           style={{ alignItems: 'center', justifyContent: 'center', flex: 1, backgroundColor: '#B0E0E6'}}>
@@ -69,7 +133,8 @@ export default function () {
           />
         </Appbar>
       </View>
-    </View>
+      </View>
+      </>
   );
 }
 const styles = StyleSheet.create({
